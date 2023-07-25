66ac7ad869d4439dd4469419e8b8c63ab763fb4c
Your Name <metsalailari@gmail.com>
2023-07-25 15:30:35 +0300

    2.11

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Person = ({ name, number }) => {
   return (
@@ -35,15 +36,19 @@ const PersonForm = ({ submit, name, number, handleName, handleNumber }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  //fetch data from server using effect hook
+  useEffect(() => { 
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const addNumber = (event) => {
     if (persons.map(a => a.name).includes(newName)) {
