97571eb7b8dd541f84072b6a60df10f9fd8bb756
Ilari Metsälä <ilari.metsala@aalto.fi>
2023-08-08 19:03:56 +0300

    4.7

--- Osa4/bloglist/utils/list_helper.js
+++ Osa4/bloglist/utils/list_helper.js
@@ -49,9 +49,33 @@ const mostBlogs = (blogs) => {
     }
 }
 
+const mostLikes = (blogs) => {
+    let best = {
+        autor: "",
+        likes: -1
+    }
+    for (let i = 0; i < blogs.length; i++) {
+        const authorName = blogs[i].author
+        const likeCount = blogs.reduce((acc, cur) => cur.author === authorName ? cur.likes + acc : acc, 0)
+
+        if (likeCount > best.likes) {
+            best = {
+                author: authorName,
+                likes: likeCount
+            }
+        }
+    }
+    if (best.likes >= 0) {
+        return best
+    } else {
+        return null
+    }
+}
+
 module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
-    mostBlogs
+    mostBlogs,
+    mostLikes
 }
