d9aa8c7423805208e863a16cc261cbef5c3eb591
Your Name <metsalailari@gmail.com>
2023-08-10 10:33:44 +0300

    4c

--- Osa4/bloglist/controllers/blogs.js
+++ Osa4/bloglist/controllers/blogs.js
@@ -1,30 +1,30 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => { 
   const blogs = await Blog.find({})
   response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const user = await User.findById(body.userId)
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: (body.likes ? body.likes : 0)
+    likes: (body.likes ? body.likes : 0),
+    user: user._id
   })
 
-  if (body.title && body.url) {
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-  } else {
-    response.status(400).json('title or url missing')
-  }
+  const savedBlog = await blog.save()
+
+  user.blogs = user.notes.concat(savedBlog._id)
+  await user.save()
+
+  response.json(savedBlog)
 })
 
 blogsRouter.get('/:id', async (request, response, next) => {
