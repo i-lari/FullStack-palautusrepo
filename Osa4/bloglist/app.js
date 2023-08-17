f3575fff090d0283effc30b6a921075232f2fab0
Your Name <metsalailari@gmail.com>
2023-08-17 12:21:00 +0300

    4.18

--- Osa4/bloglist/app.js
+++ Osa4/bloglist/app.js
@@ -6,7 +6,7 @@ const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
-
+const loginRouter = require('./controllers/login')
 
 mongoose.set('strictQuery', false)
 
@@ -24,5 +24,6 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
