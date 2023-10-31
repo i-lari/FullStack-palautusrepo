11983407ca66fcca7ab1045968a2b78d71f83c1d
Ilari Metsälä <ilari.metsala@aalto.fi>
2023-10-31 23:38:18 +0200

    5.14

--- Osa5/bloglist-fronted/src/components/Blog.test.js
+++ Osa5/bloglist-fronted/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders content', () => {
@@ -32,12 +33,39 @@ test('does not initially render url or likes', () => {
   }
   render(<Blog blog={blog}/>)
 
-  const element = screen.getByText('testUrl').closest("div")
-  const element2 = screen.getByText('2').closest("div")
-  
+  const element = screen.getByText('testUrl').closest('div')
+  const element2 = screen.getByText('2').closest('div')
+
   expect(element).toBeDefined()
   expect(element2).toBeDefined()
   expect(element2).toHaveStyle('display: none')
   expect(element).toHaveStyle('display: none')
+
+})
+
+
+test('renders url and likes after "view" -button has been pressed', async () => {
+
+  const blog = {
+    title: 'this is a test',
+    author: 'testAuthor',
+    url: 'testUrl',
+    user: 'testUser',
+    likes: '2'
+  }
+  render(<Blog blog={blog}/>)
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  expect(button).toBeDefined()
+  const element = screen.getByText('testUrl').closest('div')
+  expect(element).toBeDefined()
+  const element2 = screen.getByText('2').closest('div')
+  expect(element2).toHaveStyle('display: none')
+  expect(element).toHaveStyle('display: none')
+
+  await user.click(button)
+
+  expect(element2).toHaveStyle('display: block')
+  expect(element).toHaveStyle('display: block')
 
 })
