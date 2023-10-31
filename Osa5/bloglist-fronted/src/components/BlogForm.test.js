a1fadd3f4451f98e31a83a1bda56ea06391e2727
Ilari Metsälä <ilari.metsala@aalto.fi>
2023-11-01 01:19:59 +0200

    5.15

--- Osa5/bloglist-fronted/src/components/BlogForm.test.js
+++ Osa5/bloglist-fronted/src/components/BlogForm.test.js
@@ -6,21 +6,21 @@ import BlogForm from './BlogForm'
 
 test('<BlogForm /> calls back with correct info', async () => {
 
-    const user = userEvent.setup()
-    const createBlog = jest.fn()
+  const user = userEvent.setup()
+  const createBlog = jest.fn()
 
-    render(<BlogForm handleSubmit={createBlog}/>)
+  render(<BlogForm handleSubmit={createBlog}/>)
 
-    const inputs = screen.getAllByRole('textbox')
-    const button = screen.getByText('create')
-    expect(button).toBeDefined()
-    await user.type(inputs[0], 'formtest')
-    await user.click(button)
+  const inputs = screen.getAllByRole('textbox')
+  const button = screen.getByText('create')
+  expect(button).toBeDefined()
+  await user.type(inputs[0], 'formtest')
+  await user.click(button)
 
-    console.log(createBlog.mock.calls)
+  console.log(createBlog.mock.calls)
 
-   expect(createBlog.mock.calls).toHaveLength(1)
-   expect(createBlog.mock.calls[0][0].content).toBe('formtest')
+  expect(createBlog.mock.calls).toHaveLength(1)
+  expect(createBlog.mock.calls[0][0].content).toBe('formtest')
 
 
 })
