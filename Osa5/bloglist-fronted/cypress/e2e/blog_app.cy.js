e73ec53f7e226d9ae1a97be9f0957bb3a5071cb5
Ilari Metsälä <ilari.metsala@aalto.fi>
2023-11-05 21:46:39 +0200

    5.17

--- Osa5/bloglist-fronted/cypress/e2e/blog_app.cy.js
+++ Osa5/bloglist-fronted/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,10 @@
-describe('template spec', () => {
-  it('login page can be opened', () => {
-    cy.visit('http://localhost:3000/')
+describe('Blog app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function() {
     cy.contains('login')
   })
 })
--- Osa5/bloglist-fronted/src/App.js
+++ Osa5/bloglist-fronted/src/App.js
@@ -131,16 +131,18 @@ const App = () => {
     }
   }
 
-  const blogList = () => (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} handleLike={() => handleLike(blog.id)} showDelete={user.username === blog.user.username}
-          sortBlogs={sortBlogs}
-        />
-      )}
-    </div>
-  )
+  const blogList = () =>  {
+    return(
+      <div>
+        <h2>blogs</h2>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} handleLike={() => handleLike(blog.id)} showDelete={user.username === blog.user.username}
+            sortBlogs={sortBlogs}
+          />
+        )}
+      </div>
+    )
+  }
 
   const notifications = () => (
     <div>
