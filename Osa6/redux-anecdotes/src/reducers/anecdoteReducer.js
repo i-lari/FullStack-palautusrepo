da4f445804bada6d1ba1b8eb41b8035a5ee25b03
Your Name <metsalailari@gmail.com>
2023-11-18 09:57:45 +0200

    6.11

--- Osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ Osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import { createSlice } from '@reduxjs/toolkit'
+
 const anecdotesAtStart = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
@@ -19,39 +21,24 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
- // console.log('state now: ', state)
- // console.log('action', action)
-
-  switch(action.type) {
-    case 'VOTE' :
+const anecdotesSlice = createSlice({
+  name: 'anecdotes',
+  initialState : initialState,
+  reducers: {
+    createAnecdote(state, action) {
+      return [...state, asObject(action.payload)]
+    },
+    voteAnecdote(state, action) {
       const id = action.payload
       const anecdoteToChange = state.find( a => a.id === id)
       const changedAnecdote = {
         ...anecdoteToChange,
         votes: anecdoteToChange.votes + 1 
       }
-      return state.map(a => a.id !== id ? a : changedAnecdote).sort((a,b)=>b.votes-a.votes)
-    case 'NEW_ANECDOTE':
-      return [...state, asObject(action.payload)]
-    default:
-      return state
-  }
-}
+      return state.map(a => a.id !== id ? a : changedAnecdote).sort((a,b)=>b.votes-a.votes)  
+    }
+  },
+})
 
-export const voteAnecdote = (id) => {
-  return {
-    type:'VOTE',
-    payload: id
-  }
-}
-
-export const createAnecdote = (content) => {
-  console.log(content)
-  return {
-    type: 'NEW_ANECDOTE',
-    payload: content
-  }
-}
-
-export default reducer
+export const { createAnecdote, voteAnecdote } = anecdotesSlice.actions
+export default anecdotesSlice.reducer
--- Osa6/redux-anecdotes/src/reducers/filterReducer.js
+++ Osa6/redux-anecdotes/src/reducers/filterReducer.js
@@ -14,26 +14,3 @@ const filterSlice = createSlice({
 
 export const { filterChange } = filterSlice.actions
 export default filterSlice.reducer
-
-/*
-
-const filterReducer = (state = 'ALL', action) => {
-    switch (action.type) {
-      case 'SET_FILTER':
-        return action.payload
-      default:
-        return state
-    }
-  }
-
-  export const filterChange = ( filter ) => {
-    const ret = {
-      type: 'SET_FILTER',
-      payload: filter,
-    }
-    return ret
-  }
-
-  export default filterReducer
-
-  */
